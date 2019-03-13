318d66d9dd59cc3f31051941b41286391a13bba0
restarmilan <restarmilan@gmail.com>
2019-03-13 15:11:02 +0100

    Fixing timestamp at question details

--- server.py
+++ server.py
@@ -34,7 +34,12 @@ def save_new_question():
 def question_details(question_id):
     questions = data_manager.get_all_questions()
     question = questions[question_id]
-    return render_template('question-details.html', question=question, question_headers=data_manager.get_question_headers(), question_id=question_id,answer_headers=data_manager.get_answer_headers(), answers=util.sorting_answers_by_time())
+    readable_time = util.convert_timestamp(int(question[1]))
+    return render_template(
+        'question-details.html', question=question, question_headers=data_manager.get_question_headers(),
+        question_id=question_id, answer_headers=data_manager.get_answer_headers(),
+        answers=util.sorting_answers_by_time(), readable_time=readable_time
+    )
 
 
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
