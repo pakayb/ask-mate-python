07b0dbfdf4dc481921dd30d1df01af19884ee0d7
Pákay Barnabás <pakayb@gmail.com>
2019-04-09 15:19:33 +0200

    Refactor server

--- server.py
+++ server.py
@@ -19,28 +19,28 @@ def latest_questions(limit=5):
 @app.route('/add-question', methods=['GET', 'POST'])
 def save_new_question():
     if request.method == 'POST':
-        new_question_data = {
-            "submission_time": util.create_timestamp(),
-            "view_number": "0",
-            "vote_number": "0",
-            "title": request.form.get('title'),
-            "message": request.form.get('message'),
-        }
-        data_manager.save_new_question(new_question_data)
+        data_manager.save_new_question(create_new_question_data())
         return redirect(url_for('question_details', question_id=data_manager.get_max_id()))
     return render_template('add_question.html')
 
 
+def create_new_question_data():
+    new_question_data = {
+        "submission_time": util.create_timestamp(),
+        "view_number": "0",
+        "vote_number": "0",
+        "title": request.form.get('title'),
+        "message": request.form.get('message'),
+    }
+    return new_question_data
+
+
 @app.route('/question/<question_id>')
 def question_details(question_id):
-    questions = data_manager.get_all_questions()
-    for question in questions:
-        if int(question_id) == question.get('id'):
-            question_details = question.values()
-            view_number = question.get('view_number') + 1
-    data_manager.update_view_number(view_number)
+    question = data_manager.get_question_by_id(question_id)
+    data_manager.update_view_number(question.get('view_number'),question_id)
     return render_template(
-        'question-details.html', question=question_details,
+        'question-details.html', question=question,
         question_id=question_id,
         answers=data_manager.get_all_answers(),
         comments=data_manager.get_all_comments()
@@ -50,32 +50,31 @@ def question_details(question_id):
 @app.route('/question/<question_id>/new-answer', methods=['GET', 'POST'])
 def add_answer(question_id):
     if request.method == 'POST':
-        new_answer_data = {
-            "submission_time": util.create_timestamp(),
-            "vote_number": "0",
-            "question_id": question_id,
-            "message": request.form.get('message'),
-            "image": "n/a"
-        }
-        data_manager.save_new_answer(new_answer_data)
-        route = f"/question/{str(question_id)}"
-        return redirect(route)
+        data_manager.save_new_answer(create_new_answer_data(question_id))
+        return redirect(url_for('question_details',question_id=question_id))
     elif request.method == 'GET':
         return render_template('add_answer.html', question_id=question_id)
 
 
+def create_new_answer_data(question_id):
+    new_answer_data = {
+        "submission_time": util.create_timestamp(),
+        "vote_number": "0",
+        "question_id": question_id,
+        "message": request.form.get('message'),
+        "image": "n/a"
+    }
+    return new_answer_data
+
+
 @app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
 def update_answer(answer_id):
-    answers = data_manager.get_all_answers()
-    for answer in answers:
-        if int(answer_id) == answer.get('id'):
-            answer_message = answer.get('message')
-            question_id = answer.get('question_id')
+    answer = data_manager.get_answer_by_id(answer_id)
     if request.method == 'GET':
-        return render_template('add_answer.html', answer=answer_message, answer_id=answer_id)
+        return render_template('add_answer.html', answer=answer.get('message'), answer_id=answer.get('id'))
     elif request.method == 'POST':
         data_manager.update_answer(request.form.get('message'), answer_id)
-        return redirect(url_for('question_details', question_id=question_id))
+        return redirect(url_for('question_details', question_id=answer.get('question_id')))
 
 
 @app.route('/search')
@@ -88,35 +87,28 @@ def searching():
 @app.route('/answer/<answer_id>/new-comment', methods=['GET', 'POST'])
 def add_comment_to_answer(answer_id=None, question_id=None):
     if request.method == 'POST':
-        new_comment_data = {
-            "question_id": question_id,
-            "answer_id": answer_id,
-            "message": request.form.get('message'),
-            "submission_time": util.create_timestamp(),
-            "edited_count": 0
-        }
-        data_manager.add_comment(new_comment_data)
-        redirect_id = request.form.get('redirect_id')
-        return redirect(url_for('question_details', question_id=redirect_id))
+        data_manager.add_comment(create_new_comment_data(answer_id, question_id))
+        return redirect(url_for('question_details', question_id=request.form.get('redirect_id')))
     elif request.method == 'GET':
-        answers = data_manager.get_all_answers()
-        for answer in answers:
-            if int(answer_id) == answer.get('id'):
-                redirect_id = answer.get('question_id')
-        return render_template('add_answer_comment.html', answer_id=answer_id, redirect_id=redirect_id)
+        answer = data_manager.get_answer_by_id(answer_id)
+        return render_template('add_answer_comment.html', answer_id=answer_id, redirect_id=answer.get('question_id'))
+
+
+def create_new_comment_data(answer_id, question_id):
+    new_comment_data = {
+        "question_id": question_id,
+        "answer_id": answer_id,
+        "message": request.form.get('message'),
+        "submission_time": util.create_timestamp(),
+        "edited_count": 0
+    }
+    return new_comment_data
 
 
 @app.route('/question/<question_id>/new-comment', methods=['GET', 'POST'])
 def add_comment_to_question(answer_id=None, question_id=None):
     if request.method == 'POST':
-        new_comment_data = {
-            "question_id": question_id,
-            "answer_id": answer_id,
-            "message": request.form.get('message'),
-            "submission_time": util.create_timestamp(),
-            "edited_count": 0
-        }
-        data_manager.add_comment(new_comment_data)
+        data_manager.add_comment(create_new_comment_data(answer_id,question_id))
         return redirect(url_for('question_details', question_id=question_id))
     elif request.method == 'GET':
         return render_template('add_comment.html', question_id=question_id)
@@ -124,3 +116,4 @@ def add_comment_to_question(answer_id=None, question_id=None):
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
